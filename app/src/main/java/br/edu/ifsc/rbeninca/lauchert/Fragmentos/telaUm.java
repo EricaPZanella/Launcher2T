f66ce7d3e25055a83c637a5457048bff747c53fd
EricaPZanella <41390658+EricaPZanella@users.noreply.github.com>
2019-10-25 16:52:18 -0300

    modificacoes layout - meio add barra navigation quase lá, vai pra cada fragment

--- app/src/main/java/br/edu/ifsc/rbeninca/lauchert/Fragmentos/telaUm.java
+++ app/src/main/java/br/edu/ifsc/rbeninca/lauchert/Fragmentos/telaUm.java
@@ -1,5 +1,7 @@
 package br.edu.ifsc.rbeninca.lauchert.Fragmentos;
 
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -25,21 +27,6 @@ import br.edu.ifsc.rbeninca.lauchert.R;
  * create an instance of this fragment.
  */
 public class telaUm extends Fragment {
-    //Fragment fragmentContainer;
-
-    //@Override
-    //public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-
-      //View view = inflater.inflate(R.layout.fragment_tela_um, container, false);
-
-        //fragmentContainer = (FrameLayout) view.findViewById(R.id.fragment_tela_um);
-
-        //return view;
-   // }
-
-}
-
 //    // TODO: Rename parameter arguments, choose names that match
 //    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 //    private static final String ARG_PARAM1 = "param1";
@@ -51,9 +38,9 @@ public class telaUm extends Fragment {
 //
 //    private OnFragmentInteractionListener mListener;
 //
-//    public telaUm() {
-//        // Required empty public constructor
-//    }
+    public telaUm() {
+        // Required empty public constructor
+    }
 //
 //    /**
 //     * Use this factory method to create a new instance of
@@ -64,32 +51,33 @@ public class telaUm extends Fragment {
 //     * @return A new instance of fragment telaUm.
 //     */
 //    // TODO: Rename and change types and number of parameters
-//    public static telaUm newInstance(String param1, String param2) {
-//        telaUm fragment = new telaUm();
-//        Bundle args = new Bundle();
-//        args.putString(ARG_PARAM1, param1);
-//        args.putString(ARG_PARAM2, param2);
-//        fragment.setArguments(args);
-//        return fragment;
-//    }
-//
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        if (getArguments() != null) {
-//            mParam1 = getArguments().getString(ARG_PARAM1);
-//            mParam2 = getArguments().getString(ARG_PARAM2);
-//        }
-//    }
-//
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                             Bundle savedInstanceState) {
-//        // Inflate the layout for this fragment
-//        return inflater.inflate(R.layout.fragment_telaUm, container, false);
-//    }
-//
-//    // TODO: Rename method, update argument and hook method into UI event
+    public static telaUm newInstance(String param1, String param2) {
+        telaUm fragment = new telaUm();
+        Bundle args = new Bundle();
+        //args.putString(ARG_PARAM1, param1);
+        //args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            //mParam1 = getArguments().getString(ARG_PARAM1);
+            //mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tela_um, container, false);
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    // TODO: Renomeie o método, atualize o argumento e ganhe o método no evento da interface do usuário
 //    public void onButtonPressed(Uri uri) {
 //        if (mListener != null) {
 //            mListener.onFragmentInteraction(uri);
@@ -106,25 +94,25 @@ public class telaUm extends Fragment {
 //                    + " must implement OnFragmentInteractionListener");
 //        }
 //    }
-//
+
 //    @Override
 //    public void onDetach() {
 //        super.onDetach();
 //        mListener = null;
 //    }
-//
-//    /**
-//     * This interface must be implemented by activities that contain this
-//     * fragment to allow an interaction in this fragment to be communicated
-//     * to the activity and potentially other fragments contained in that
-//     * activity.
-//     * <p>
-//     * See the Android Training lesson <a href=
-//     * "http://developer.android.com/training/basics/fragments/communicating.html"
-//     * >Communicating with Other Fragments</a> for more information.
-//     */
-//    public interface OnFragmentInteractionListener {
-//        // TODO: Update argument type and name
-//        void onFragmentInteraction(Uri uri);
-//    }
-//}
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
--- app/src/main/java/br/edu/ifsc/rbeninca/lauchert/MainActivity.java
+++ app/src/main/java/br/edu/ifsc/rbeninca/lauchert/MainActivity.java
@@ -1,7 +1,10 @@
 package br.edu.ifsc.rbeninca.lauchert;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -20,15 +23,25 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.bottomnavigation.BottomNavigationView.OnNavigationItemSelectedListener;
 
 import java.util.ArrayList;
-public class MainActivity extends AppCompatActivity {
+
+import br.edu.ifsc.rbeninca.lauchert.Fragmentos.telaTres;
+import br.edu.ifsc.rbeninca.lauchert.Fragmentos.telaUm;
+import br.edu.ifsc.rbeninca.lauchert.Fragmentos.tela_dois;
+
+import static br.edu.ifsc.rbeninca.lauchert.R.color.colorAccent;
+import static br.edu.ifsc.rbeninca.lauchert.R.color.colorPrimary;
+
+public class MainActivity extends AppCompatActivity implements OnNavigationItemSelectedListener{
     RecyclerView recyclerView;
     LaucherControler laucherControler;
     EditText editTextPesquisa;
     ArrayList<AppInfo> aplicativosList;
     AppInfoArrayAdapter mAdapter;
     BottomNavigationView bottomNavigationView;
+    ActionBar actionBar;
     private RecyclerView.LayoutManager layoutManager;
     String [] listaPacoteAppTela1 = {"com.google.android.dialer","com.google.android.apps.messaging","com.android.contacts", "com.google.android.deskclock", "com.android.calculator2", "com.google.android.music", "com.google.android.apps.maps"};
     String [] listaRenaimeTela1 = {"Chamadas","Mensagens","Contatos", "Despertador", "Câmera", "Reprodutor de música"};
@@ -46,36 +59,10 @@ public class MainActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.recycleView);
         //editTextPesquisa = findViewById(R.id.editTextKeyWord);
 
+        actionBar = getSupportActionBar();
         bottomNavigationView = findViewById(R.id.bottom_navigation);
-
-        bottomNavigationView.setOnNavigationItemReselectedListener(
-                new BottomNavigationView.OnNavigationItemReselectedListener() {
-                    @Override
-                    public void onNavigationItemReselected(@NonNull MenuItem menuItem) {
-
-                        switch (menuItem.getItemId()){
-                            case R.id.nav_setaesquerda:{
-
-
-
-                            }
-                            case R.id.nav_telainicial:{
-
-
-                                
-                            }
-                            case R.id.nav_setadireita:{
-
-
-                                
-                            }
-                        }
-
-                    }
-
-                    private void refreshList(String s) {
-                    }
-                });
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        loadFragment(new telaUm());
 
 
         //Inicialização do controlador do Laucher
@@ -119,7 +106,6 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-
     public void refreshList(String key ){
         aplicativosList=laucherControler.loadAppInf(key);
         mAdapter  = new AppInfoArrayAdapter(getApplicationContext(),
@@ -153,4 +139,40 @@ public class MainActivity extends AppCompatActivity {
     };
 
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        Fragment fragment;
+
+        switch (menuItem.getItemId()){
+                case R.id.nav_setaesquerda:
+                    //bottomNavigationView.setItemBackgroundResource(colorAccent);
+                    fragment = new tela_dois();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.nav_telainicial:
+                    //bottomNavigationView.setItemBackgroundResource(colorAccent);
+                    fragment = new telaUm();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.nav_setadireita:
+                    //bottomNavigationView.setItemBackgroundResource(colorAccent);
+                    fragment = new telaTres();
+                    loadFragment(fragment);
+                    return true;
+
+        }
+
+       return false;
+    }
+
+
+    private void loadFragment(Fragment fragment){
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.container, fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+
+    }
 }
